8c4c7986ecb6007d3db255f349aae36a060c9b88
Deep Trivedi <deepbtrivedi04@gmail.com>
2025-01-26 02:46:46 +0530

    multithreaded server exec

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+use std::fs;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
+    // Correct bind address
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4); // Create a thread pool with 4 threads
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);  // Use mutable reference here
-    let http_request: Vec<_> = buf_reader
+    // Read the HTTP request
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader
         .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+        .next()
+        .unwrap()
+        .unwrap(); // Read the first line of the request
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hellow.html").unwrap();
+    // Match the request line to determine the response
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hellow.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hellow.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    // Read the contents of the file
+    let contents = fs::read_to_string(filename).unwrap_or_else(|_| String::from("File not found"));
     let length = contents.len();
 
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    );
+    // Construct the HTTP response
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
+    // Send the response to the client
     stream.write_all(response.as_bytes()).unwrap();
 }
